53d0dcb55015f52a13324d585af42645736841b0
Larry Song <1631094+songlining@users.noreply.github.com>
2025-06-16 11:40:10 +1000

    start using private module

---
+++ main.tf
@@ -0,0 +1,30 @@
+# Basic example of using the Vault AWS authentication module
+
+terraform {
+  cloud {
+    organization = "lab-larry"
+    workspaces {
+      name = "tfc-demo"
+    }
+  }
+}
+
+# module "vault_aws_auth" {
+#   source = "git@github.com:songlining/terraform-aws-vault-agent-auth-role.git"
+#   # Using default values for simplicity
+# }
+
+module "vault-agent-auth-role" {
+  source  = "app.terraform.io/lab-larry/vault-agent-auth-role/aws"
+  version = "1.0.7"
+}
+
+output "vault_server_private_ip" {
+  value = module.vault_aws_auth.vault_server_private_ip
+  description = "Private IP address of the Vault server"
+}
+
+output "vault_agent_public_ip" {
+  value = module.vault_aws_auth.vault_agent_public_ip
+  description = "Public IP address of the Vault agent"
+}
