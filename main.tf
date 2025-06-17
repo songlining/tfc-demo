411eb3a668861ed0ae368153c00386a66161c361
Larry Song <1631094+songlining@users.noreply.github.com>
2025-06-17 10:07:06 +1000

    test

--- main.tf
+++ main.tf
@@ -9,26 +9,26 @@ terraform {
   }
 }
 
-module "vault-agent-auth-role" {
-  source = "github.com/songlining/terraform-aws-vault-agent-auth-role.git"
-    
-  aws_region  = "ap-southeast-2"
-  # aws_region  = "ap-southeast-1"
-  instance_type = "t4g.small"
-  # instance_type = "t4g.micro"
-  # instance_type = "t4g.medium"
-}
-
 # module "vault-agent-auth-role" {
-#   source  = "app.terraform.io/lab-larry/vault-agent-auth-role/aws"
-#   version = "1.0.9"
-
+#   source = "github.com/songlining/terraform-aws-vault-agent-auth-role.git"
+    
 #   aws_region  = "ap-southeast-2"
 #   # aws_region  = "ap-southeast-1"
 #   instance_type = "t4g.small"
 #   # instance_type = "t4g.micro"
 #   # instance_type = "t4g.medium"
 # }
+
+module "vault-agent-auth-role" {
+  source  = "app.terraform.io/lab-larry/vault-agent-auth-role/aws"
+  version = "1.0.9"
+
+  aws_region  = "ap-southeast-2"
+  # aws_region  = "ap-southeast-1"
+  instance_type = "t4g.small"
+  # instance_type = "t4g.micro"
+  # instance_type = "t4g.medium"
+}
 
 output "vault_server_private_ip" {
   value = module.vault-agent-auth-role.vault_server_private_ip
